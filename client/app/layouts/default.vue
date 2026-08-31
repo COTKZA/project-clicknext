9e9b15f8021367903827a8dd7fa864aa2decdd13
cotkza <cotkgtasa123@gmail.com>
2026-08-31 18:35:30 +0700

    update ui login

---
+++ client/app/layouts/default.vue
@@ -0,0 +1,15 @@
+<script setup lang="ts">
+import Navbar from '~/components/Navbar.vue';
+
+</script>
+
+<template>
+  <div>
+    <div >
+        <Navbar />
+      <main>
+        <slot />
+      </main>
+    </div>
+  </div>
+</template>
