290fdf73bd13e9952bb2be0b91edf2309406f646
cotkza <cotkgtasa123@gmail.com>
2026-09-01 06:37:50 +0700

    update function delete and edit transactionById

--- client/app/components/transactions/EditTransactionModa.vue
+++ client/app/components/transactions/EditTransactionModa.vue
@@ -1,53 +1,125 @@
-<script setup>
-import { ref } from 'vue';
+<script setup lang="ts">
+import { ref } from "vue";
+import { useTransactionStore, type Transaction } from "~/stores/transactions";
 
-const isOpen = ref(false)
+const props = defineProps<{
+  id: number | null;
+}>();
 
-const open = () => (isOpen.value = true)
-const close = () => (isOpen.value = false)
+const transactionStore = useTransactionStore();
 
-defineExpose({ open, close })
-
+const isOpen = ref(false);
+const errorMessage = ref("");
+const transactionById = ref<Transaction>();
 const amount = ref();
 
+const open = () => {
+  if (!props.id) return;
+
+  transactionById.value = transactionStore.getTransactionById(Number(props.id));
+  amount.value = transactionById.value?.amount ?? 0;
+
+  isOpen.value = true;
+  errorMessage.value = "";
+};
+const close = () => {
+  isOpen.value = false;
+  errorMessage.value = "";
+};
+
+defineExpose({ open, close });
+
 // validate amount
-const handleInputAmount = (e) => {
-    const value = parseFloat(e.target.value);
-    if (value > 100000) {
-        amount.value = 100000
-    }
-}
+const handleInputAmount = () => {
+  const value = parseFloat(amount.value);
+  if (value > 100000) {
+    alert("โดยสามารถกรอกได้เฉพาะตัวเลข ไม่เกิน 0-100,000 บาท");
+    return;
+  }
+};
 
+const onSubmit = () => {
+  if (!props.id) return;
+
+  if (!amount.value || amount.value < 0) {
+    alert("กรุณากรอกจำนวนเงิน");
+    return;
+  }
+
+  try {
+    transactionStore.updateTransactionById(Number(props.id), Number(amount));
+    close();
+  } catch (error: any) {
+    alert(error.message);
+  }
+};
 </script>
+
 <template>
-    <div v-if="isOpen" class="fixed inset-0 z-50 flex items-center justify-center bg-black/20">
-        <div class="w-full max-w-md overflow-hidden rounded-lg bg-white p-6 border border-gray-300 mx-3">
-            <div class="flex flex-col gap-2">
-                <h2 class="text-lg font-bold">แก้ไขจำนวนเงินฝาก</h2>
-                <form action="">
-                    <div class="flex flex-col">
-                        <span class="text-md font-medium">ของวันที่ 05/02/2022 10:10:53</span>
-                        <span class="text-md font-medium">จากอีเมล admin@gmail.com</span>
-                    </div>
+  <div
+    v-if="isOpen"
+    class="fixed inset-0 z-50 flex items-center justify-center bg-black/20"
+  >
+    <div
+      class="w-full max-w-md overflow-hidden rounded-lg bg-white p-6 border border-gray-300 mx-3"
+    >
+      <div class="flex flex-col gap-2">
+        <h2 class="text-lg font-bold">แก้ไขจำนวนเงินฝาก</h2>
+        <form @submit.prevent="onSubmit">
+          <div class="flex flex-col">
+            <span class="text-md font-medium"
+              >ของวันที่ {{ transactionById?.dateTime }}</span
+            >
+            <span class="text-md font-medium"
+              >จากอีเมล {{ transactionById?.email }}</span
+            >
+          </div>
 
-                    <div>
-                        <label for="amount" class="block mb-2 text-sm font-medium text-gray-900">
-                            จำนวนเงิน *
-                        </label>
-                        <input type="number" name="amount" id="amount" v-model.number="amount" min="1" max="100000"
-                            step="1" inputmode="decimal" pattern="[0-9]" @input="handleInputAmount"
-                            class="bg-white border-2 border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-gray-200 focus:border-gray-400 block w-full p-1"
-                            placeholder="กรอกจำนวนเงิน" required />
-                    </div>
+          <div>
+            <label
+              for="amount"
+              class="block mb-2 text-sm font-medium text-gray-900"
+            >
+              จำนวนเงิน *
+            </label>
+            <input
+              type="number"
+              name="amount"
+              id="amount"
+              v-model.number="amount"
+              min="1"
+              max="100000"
+              step="1"
+              inputmode="decimal"
+              pattern="[0-9]"
+              @input="handleInputAmount"
+              class="bg-white border-2 border-gray-300 text-gray-900 rounded-lg focus:outline-none focus:ring-gray-200 focus:border-gray-400 block w-full p-1"
+              placeholder="กรอกจำนวนเงิน"
+              required
+            />
+          </div>
 
-                    <div class="flex items-center justify-start mt-4">
-                        <button type="submit"
-                            class="bg-[#4d4d4d] text-white px-4 py-1.5 rounded-md font-medium">ยืนยัน</button>
-                        <button type="button" @click="close"
-                            class="text-gray-900 px-4 py-1.5 rounded-md font-medium">ยกเลิก</button>
-                    </div>
-                </form>
-            </div>
-        </div>
+          <p v-if="errorMessage" class="text-xs text-red-500">
+            {{ errorMessage }}
+          </p>
+
+          <div class="flex items-center justify-start mt-4">
+            <button
+              type="submit"
+              class="bg-[#4d4d4d] text-white px-4 py-1.5 rounded-md font-medium"
+            >
+              ยืนยัน
+            </button>
+            <button
+              type="button"
+              @click="close"
+              class="text-gray-900 px-4 py-1.5 rounded-md font-medium"
+            >
+              ยกเลิก
+            </button>
+          </div>
+        </form>
+      </div>
     </div>
+  </div>
 </template>
--- client/app/stores/transactions.ts
+++ client/app/stores/transactions.ts
@@ -2,30 +2,95 @@ import { defineStore } from "pinia";
 
 export interface Transaction {
   id: number;
-  dateTime: string;
+  dateTime: string | any;
   amount: number;
   status: "deposit" | "withdraw";
   email: string;
 }
 
 export const useTransactionStore = defineStore("transactions", {
-    
   state: () => ({
-    data: [
-      {
-        id: 1,
-        dateTime: "2026-08-31 10:30:00",
-        amount: 5000,
-        status: "deposit",
-        email: "test1234@gmail.com",
-      },
-      {
-        id: 2,
-        dateTime: "2026-08-31 10:30:00",
-        amount: 3000,
-        status: "withdraw",
-        email: "test1234@gmail.com",
-      },
-    ] as Transaction[],
+    data: [] as Transaction[],
   }),
+
+  // fn balance
+  getters: {
+    balance: (state) => {
+      let total = 0;
+
+      state.data.forEach((transactions) => {
+        if (transactions.status === "deposit") {
+          total += transactions.amount;
+        } else {
+          total -= transactions.amount;
+        }
+      });
+
+      return total;
+    },
+  },
+
+  actions: {
+    // create
+    addTransaction(
+      status: "deposit" | "withdraw",
+      amount: number,
+      email: string,
+    ) {
+      // validate amount
+      if (amount < 0 || amount > 100000) {
+        throw new Error("จำนวนเงินต้องอยู่ระหว่าง 0 - 100,000 บาท");
+      }
+
+      // check balance
+      if (status === "withdraw" && amount > this.balance) {
+        throw new Error("ยอดเงินคงเหลือไม่เพียงพอ");
+      }
+
+      const transaction: Transaction = {
+        id: this.data.length + 1,
+        dateTime: new Date().toLocaleDateString("th-TH", {
+          day: "2-digit",
+          month: "short",
+          year: "numeric",
+          hour: "2-digit",
+          minute: "2-digit",
+          second: "2-digit",
+          timeZone: "Asia/Bangkok",
+        }),
+        amount: amount,
+        status: status,
+        email: email,
+      };
+
+      this.data.push(transaction);
+    },
+
+    // find id
+    getTransactionById(id: number) {
+      const transaction = this.data.find((item) => item.id === id);
+      return transaction;
+    },
+
+    updateTransactionById(id: number, amount: number) {
+      const transaction = this.data.find((item) => item.id === id);
+
+      if (!transaction) {
+        throw new Error("ไม่พบ Id ของ Transaction");
+      }
+
+      transaction.amount -= amount
+    },
+
+    // delete
+    deleteTransaction(id: number) {
+      const transaction = this.data.find((item) => item.id === id);
+
+      if (!transaction) {
+        throw new Error("ไม่พบ Id ของ Transaction");
+      }
+
+      this.data = this.data.filter((transaction) => transaction.id !== id);
+    },
+  },
 });
