eaea7d398f4e11f804229fad5ca3f72309126504
cotkza <cotkgtasa123@gmail.com>
2026-09-02 18:41:13 +0700

    delete h1

--- client/app/components/auth/LoginForm.vue
+++ client/app/components/auth/LoginForm.vue
@@ -37,7 +37,6 @@ const signIn = async () => {
 <template>
   <div class="w-full sm:max-w-md">
     <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
-      <!-- <h1 class="font-bold text-center text-xl text-gray-900">Login</h1> -->
       <form @submit.prevent="signIn" class="space-y-4">
         <div>
           <label
